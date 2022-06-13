58db16279c294a6e7778a9a0a023d3488d5a248c
Martin <martin@57blocks.com>
2022-06-13 18:46:07 +0800

    support transfer and swap

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import logo from "./logo.svg";
+// @ts-nocheck
 import "./App.css";
-import React, { FC, useMemo, useCallback } from "react";
+
+import {
+  Liquidity,
+  Percent,
+  Token,
+  TokenAmount,
+} from "@raydium-io/raydium-sdk";
+import { createTransferCheckedInstruction } from "@solana/spl-token";
+import {
+  WalletAdapterNetwork,
+  WalletNotConnectedError,
+} from "@solana/wallet-adapter-base";
 import {
   ConnectionProvider,
+  useConnection,
+  useWallet,
   WalletProvider,
 } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  WalletDisconnectButton,
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
 import {
   BitKeepWalletAdapter,
   BitpieWalletAdapter,
@@ -26,21 +43,26 @@ import {
   TorusWalletAdapter,
 } from "@solana/wallet-adapter-wallets";
 import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from "@solana/wallet-adapter-react-ui";
-import { clusterApiUrl } from "@solana/web3.js";
-import { WalletNotConnectedError } from "@solana/wallet-adapter-base";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+  clusterApiUrl,
+  Keypair,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+import React, { FC, useCallback, useMemo } from "react";
+
+import { getOrCreateAssociatedTokenAccount } from "./transfer";
+import { getTokenAccountsByOwner } from "./util";
+import { fetchDevPoolKeys } from "./util_devnet";
+
+// import { Token as SplToken } from "@solana/spl-token";
 
 // Default styles that can be overridden by your app
 require("@solana/wallet-adapter-react-ui/styles.css");
 
 function App() {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Mainnet;
+  const network = WalletAdapterNetwork.Devnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
@@ -77,7 +99,9 @@ function App() {
         <WalletModalProvider>
           <WalletMultiButton />
           <WalletDisconnectButton />
-          <SendOneLamportToRandomAddress />
+          <SendLamports />
+          <TransferSplToken />
+          <LiquiditySwap />
         </WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
@@ -86,7 +110,7 @@ function App() {
 
 export default App;
 
-export const SendOneLamportToRandomAddress: FC = () => {
+export const SendLamports: FC = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction } = useWallet();
 
@@ -102,7 +126,7 @@ export const SendOneLamportToRandomAddress: FC = () => {
     );
 
     const signature = await sendTransaction(transaction, connection);
-
+    console.log(signature);
     await connection.confirmTransaction(signature, "processed");
   }, [publicKey, sendTransaction, connection]);
 
@@ -112,3 +136,188 @@ export const SendOneLamportToRandomAddress: FC = () => {
     </button>
   );
 };
+
+export const LiquiditySwap: FC = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const liquiditySwap = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+    const wSOL = {
+      address: "So11111111111111111111111111111111111111112",
+      decimals: 9,
+    };
+    const USDT = {
+      address: "8FRFC6MoGGkMFQwngccyu69VnYbzykGeez7ignHVAFSN",
+      decimals: 6,
+    };
+    const token0 = wSOL;
+    const token1 = USDT;
+    const amountIn = 10000000;
+    // SOL_USDT POOL DEV ammId
+    const SOL_USDT_AMMM_ID = "384zMi9MbUKVUfkUdrnuMfWBwJR9gadSxYimuXeJ9DaJ";
+
+    // FIDA-SOL POOL DEV ammId
+    const FIDA_SOL = "ER3u3p9TGyA4Sc9VCJrkLq4hR73GFW8QAzYkkz8rSwsk";
+    // RAY_USDC POOL DEV ammId
+    // const RAY_USDC = "ELSGBb45rAQNsMTVzwjUqL8vBophWhPn4rNbqwxenmqY"
+
+    // const POOL_ID = "58oQChx4yWmvKdwLLZzBi4ChoCc2fqCUWBkwMihLYQo2";
+
+    const POOL_ID = SOL_USDT_AMMM_ID;
+    // const allPoolKeys = await fetchAllPoolKeys(connection);
+    // const poolKeys = allPoolKeys.find((item) => item.id.toBase58() === RAY_USDC)
+    const poolKeys = await fetchDevPoolKeys(connection, new PublicKey(POOL_ID));
+    // const poolKeys = await fetchPoolKeys(connection, new PublicKey(POOL_ID));
+
+    console.log(poolKeys.baseMint.toBase58());
+
+    console.log(poolKeys.quoteMint.toBase58());
+    const poolInfo = await Liquidity.fetchInfo({ connection, poolKeys });
+    console.log(poolInfo);
+
+    const owner = publicKey;
+    const tokenAccounts = await getTokenAccountsByOwner(connection, owner);
+    // real amount = 1000000 / 10**poolInfo.baseDecimals
+    const tokenAmountIn = new TokenAmount(
+      new Token(new PublicKey(token0.address), token0.decimals),
+      amountIn,
+      true
+    );
+
+    const currencyOut = new Token(
+      new PublicKey(token1.address),
+      token1.decimals
+    );
+
+    // 5% slippage
+    const slippage = new Percent(5, 100);
+
+    const {
+      amountOut,
+      minAmountOut,
+      currentPrice,
+      executionPrice,
+      priceImpact,
+      fee,
+    } = Liquidity.computeAmountOut({
+      poolKeys,
+      poolInfo,
+      amountIn: tokenAmountIn,
+      currencyOut,
+      slippage,
+    });
+
+    // @ts-ignore
+    // console.log(amountOut.toFixed(), minAmountOut.toFixed(), currentPrice.toFixed(), executionPrice.toFixed(), priceImpact.toFixed(), fee.toFixed())
+    console.log(
+      `swap: ${poolKeys.id.toBase58()}, amountIn: ${tokenAmountIn.toFixed()}, amountOut: ${amountOut.toFixed()}, executionPrice: ${executionPrice?.toFixed()}`
+    );
+
+    // const minAmountOut = new TokenAmount(new Token(poolKeys.quoteMint, poolInfo.quoteDecimals), 1000000)
+
+    const { transaction, signers } = await Liquidity.makeSwapTransaction({
+      connection,
+      poolKeys,
+      userKeys: {
+        tokenAccounts,
+        owner,
+      },
+      amountIn: tokenAmountIn,
+      amountOut: minAmountOut,
+      fixedSide: "in",
+    });
+
+    // await sendTx(connection, transaction, [ownerKeypair, ...signers]);
+    const txid = await sendTransaction(transaction, connection, {
+      signers,
+      skipPreflight: true,
+    });
+    console.log(`swap end: ${txid}`);
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <button onClick={liquiditySwap} disabled={!publicKey}>
+      Liquidity swap
+    </button>
+  );
+};
+
+export const TransferSplToken: FC = () => {
+  const { connection } = useConnection();
+  const { publicKey, sendTransaction } = useWallet();
+
+  const liquiditySwap = useCallback(async () => {
+    if (!publicKey) throw new WalletNotConnectedError();
+
+    const mint = new PublicKey("8FRFC6MoGGkMFQwngccyu69VnYbzykGeez7ignHVAFSN");
+    const to = new PublicKey("7m1pg7NGCazuLc8RQQdmsujEp9VeificdcDNsYRVFXzh");
+    const amount = 1000000;
+    const decimals = 6;
+    try {
+      const fromTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        publicKey,
+        mint,
+        publicKey,
+        sendTransaction,
+        "confirmed"
+      );
+
+      const toTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        publicKey,
+        mint,
+        to,
+        sendTransaction,
+        "confirmed"
+      );
+
+      const transaction = new Transaction().add(
+        createTransferCheckedInstruction(
+          fromTokenAccount.address, // source
+          mint,
+          toTokenAccount.address, // dest
+          publicKey,
+          amount,
+          decimals
+        )
+      );
+
+      // step 2: sign&send transaction
+      const txid = await sendTransaction(transaction, connection);
+
+      console.log(`transfer hash: ${txid}`);
+
+      const latestBlockHash = await connection.getLatestBlockhash();
+
+      const result = await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: txid,
+      });
+      if (result.context.slot) {
+        return true;
+      }
+      console.log(`result: ${JSON.stringify(result)}`);
+    } catch (err) {
+      // err handle
+    }
+  }, [publicKey, sendTransaction, connection]);
+
+  return (
+    <button onClick={liquiditySwap} disabled={!publicKey}>
+      Transfer Spl
+    </button>
+  );
+};
+
+export const token0 = {
+  address: "So11111111111111111111111111111111111111112",
+  decimals: 9,
+};
+export const token1 = {
+  address: "8FRFC6MoGGkMFQwngccyu69VnYbzykGeez7ignHVAFSN",
+  decimals: 9,
+};
+export const amountIn = 1000000;
